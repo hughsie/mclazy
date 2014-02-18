dbfdee67dd703dbaf008917a1a068ce1c4766d66
Richard Hughes <richard@hughsie.com>
2014-02-18 08:09:42 +0000

    Use less of copr.subcommands as we need to detect failure

--- copr.py
+++ copr.py
@@ -21,6 +21,8 @@
 import koji
 import sqlite3
 import time
+import requests
+
 import copr_cli.subcommands
 from xml.etree.ElementTree import ElementTree
 
@@ -56,10 +58,9 @@ class Db:
     def __init__(self):
         self.con = sqlite3.connect(MCLAZY_COPR_DATABASE)
         cur = self.con.cursor()
-        try:
-            cur.execute("CREATE TABLE builds(timestamp INT, nvr TEXT)")
-        except sqlite3.OperationalError:
-            pass
+        cur.execute("CREATE TABLE IF NOT EXISTS builds(timestamp INTEGER PRIMARY KEY ASC, nvr TEXT)")
+        cur.close()
+        self.con.commit()
 
     def __del__(self):
         self.con.close()
@@ -68,14 +69,18 @@ class Db:
         cur = self.con.cursor()
         cur.execute("SELECT * FROM builds WHERE nvr = '%s'" % pkg.get_nvr())
         rows = cur.fetchall()
+        cur.close()
         return len(rows) > 0
 
     def add_build(self, pkg):
         if self.build_exists(pkg):
+            print "pkg already exists!"
             return;
         cur = self.con.cursor()
         ts = int(time.time())
-        cur.execute("INSERT INTO builds VALUES(%i,'%s')" % (ts, pkg.get_nvr()))
+        cur.execute("INSERT INTO builds(timestamp,nvr) VALUES(?,?)", (ts, pkg.get_nvr()))
+        cur.close()
+        self.con.commit()
 
 class Koji:
     def __init__(self):
@@ -89,6 +94,60 @@ class Koji:
         pkg.version = latest['version']
         pkg.release = latest['release']
         return pkg
+
+def build_in_copr(copr, pkgs, wait=True):
+    """ Build a new package into a given copr. """
+    user = copr_cli.subcommands.get_user()
+    copr_api_url = copr_cli.subcommands.get_api_url()
+    URL = '{0}/coprs/{1}/{2}/new_build/'.format(
+        copr_api_url,
+        user['username'],
+        copr)
+
+    data = {'pkgs': ' '.join(pkgs),
+            'memory': None,
+            'timeout': None
+            }
+
+    req = requests.post(URL,
+                        auth=(user['login'], user['token']),
+                        data=data)
+    output = copr_cli.subcommands._get_data(req, user, copr)
+    if output is None:
+        return False
+    else:
+        print_info(output['message'])
+
+    if wait:
+        print_info("Watching build: %i" % output['id'])
+        prevstatus = None
+        try:
+            while True:
+                (ret, status) = copr_cli.subcommands._fetch_status(output['id'])
+                if not ret:
+                    print_fail("Unable to get build status")
+                    return False
+
+                if prevstatus != status:
+                    prevstatus = status
+
+                if status == 'succeeded':
+                    return True
+                if status == 'failed':
+                    return False
+
+                time.sleep(10)
+
+        except KeyboardInterrupt:
+            pass
+
+    return True
+
+def print_info(text):
+    print COLOR_OKBLUE + "    INFO: " + text + COLOR_ENDC
+
+def print_fail(text):
+    print COLOR_FAIL + "    FAILED: " + text + COLOR_ENDC
 
 def main():
 
@@ -109,22 +168,22 @@ def main():
 
     for pkg_name in pkg_names:
 
-        print COLOR_OKBLUE + "    INFO: Looking for " + pkg_name + COLOR_ENDC
+        print_info("Looking for %s" % pkg_name)
 
         # get the latest build from koji
         pkg = koji.get_newest_build(pkg_name)
-        print COLOR_OKBLUE + "    INFO: Latest version " + pkg.get_nvr() + COLOR_ENDC
+        print_info("Latest version " + pkg.get_nvr())
 
         # has this build been submitted?
         if db.build_exists(pkg):
-            print COLOR_OKBLUE + "    INFO: Already build in copr!" + COLOR_ENDC
+            print_info("Already build in copr!")
             continue
 
         # submit to copr
-        print COLOR_OKBLUE + "    INFO: Submitting URL " + pkg.get_url() + COLOR_ENDC
-        rc = copr_cli.subcommands.build(MCLAZY_COPR_ID, [pkg.get_url()], None, None)
+        print_info("Submitting URL " + pkg.get_url())
+        rc = build_in_copr(MCLAZY_COPR_ID, [pkg.get_url()])
         if rc != True:
-            print COLOR_FAIL + "    FAILED: build" + COLOR_ENDC
+            print_fail("build")
             break
 
         # add to database
