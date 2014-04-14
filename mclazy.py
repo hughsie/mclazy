7138ab6de4507d86c865ca0faa7fb80e1273a5e8
Richard Hughes <richard@hughsie.com>
2014-04-14 10:24:45 +0100

    trivial: Remove some unused constants

--- mclazy.py
+++ mclazy.py
@@ -33,13 +33,6 @@ import fnmatch
 from modules import ModulesXml
 from log import print_debug, print_info, print_fail
 
-COLOR_HEADER = '\033[95m'
-COLOR_OKBLUE = '\033[94m'
-COLOR_OKGREEN = '\033[92m'
-COLOR_WARNING = '\033[93m'
-COLOR_FAIL = '\033[91m'
-COLOR_ENDC = '\033[0m'
-
 def run_command(cwd, argv):
     print_debug("Running %s" % " ".join(argv))
     p = subprocess.Popen(argv, cwd=cwd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
