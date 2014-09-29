22c6efea89bab2f585eaf8fc0e21bf481274f2db
Richard Hughes <richard@hughsie.com>
2014-09-29 16:15:41 +0100

    Add helper script that I use

---
+++ helpers/run-f20-gnome-3-12.sh
@@ -0,0 +1,2 @@
+#./mclazy.py --fedora-branch f20-gnome-3-12 --cache /home/hughsie/Work/Fedora/ --simulate
+./mclazy.py --copr-id f20-gnome-3-12 --fedora-branch f20-gnome-3-12 --cache /home/hughsie/Work/Fedora/
